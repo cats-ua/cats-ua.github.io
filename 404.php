27e57fd3ca28d8232def12584b7c0e7863df5ca6
andreybredli <67146803+andreybredli@users.noreply.github.com>
2021-03-31 17:56:25 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,80 @@
 <?php
 header ("HTPP/1.0 404 Not Found");
 ?>
+<html class="no-js" lang="ru">
+<!--<![endif]-->
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V251V2BBPR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V251V2BBPR');
+</script>
+
+<meta charset="utf-8">
+<meta name="robots" content="index,follow">
+<!-- Yandex index -->
+<meta name="yandex-verification" content="bdb49550db8794db" />
+<meta name="description" content="Картинки милых котиков их характеристика и многое другое в нашем блоге">
+<meta name="keywords" content="котики,картинки,милые,характеристика,пушистики,блог">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/flexslider.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/font-icon.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+<!-- header section -->
+<section class="banner" role="banner">
+  <header id="header">
+    <div class="header-content clearfix" > <span class="logo"><a>Cats.ua</a></span>
+      <nav class="navigation">
+        <ul class="primary-nav">
+          <li><a href="about.html">О нас</a></li>
+          <li><a href="#cats">Коты</a></li>
+          <li><a href="#gallery">Галерея</a></li>
+        </ul>
+      </nav>
+      
+    </div>
+  </header>
+  <!-- banner text -->
+  <div class="container">
+    <div class="col-md-6 col-sm-12">
+      <div class="banner-text text-center">
+        <h1>Котики</h1>
+        <p>Зарядитесь милотой и позитовом<br/> </p></div>
+      <!-- banner text --> 
+    </div>
+  </div>
+</section>
+<!-- Footer section -->
+<footer class="footer">
+<div class="container-fluid">
+<div id="map-row" class="row">
+    <div class="">    
+      <iframe width="5" height="5" ></iframe>      
+    	</div>
+    </div>
+	 </div>
+</footer>
+<!-- Footer section --> 
+<!-- JS FILES --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/jquery.flexslider-min.js"></script> 
+<script src="js/jquery.fancybox.pack.js"></script> 
+<script src="js/retina.min.js"></script> 
+<script src="js/modernizr.js"></script> 
+<script src="js/main.js"></script> 
+<script src="js/jquery.contact.js"></script> 
+</body>
+</html>
