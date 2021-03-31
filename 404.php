156f6dd39d27a7c70369bb602481a3c3920694f0
andreybredli <67146803+andreybredli@users.noreply.github.com>
2021-03-31 18:14:20 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -3,6 +3,7 @@ header ("HTPP/1.1 404 Not Found");
 ?>
 <!DOCTYPE html>
 <html>
+   
     <head>
         <title>Erorr </title>
         <meta http-equiv="Content-Type" content="text/html; charset-windows-1251">
@@ -10,6 +11,6 @@ header ("HTPP/1.1 404 Not Found");
     <body style="background:#ccc;">
     <h1>Erorr404</h1>
     <a href="/">Homepage</a>
-    </body
+    </body>
  </html>
 
